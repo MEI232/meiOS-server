9f82af76e84dec465bb6276a7ab52f20f88ff0b9
MEI232 <100521891+MEI232@users.noreply.github.com>
2022-07-16 15:49:55 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+if(version == '1.7'){
+  
+}else{
+  swal('Updated', 'Updated to 1.7', "success")
+  var meowx = document.createElement('script');
+meowx.src = 'server.js';
+document.body.appendChild(meowx);
+  version = '1.7'
+}
+addResult('', 'Pinged MEI232os server')
+//alert('hello')
+if(username == 'Thomas'){
+unmute()
+}
